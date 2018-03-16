65438a0d9c0ad8e5356fab094b5c3ec20093aa52
Michael Lore <michael.lore@gmail.com>
2018-03-16 12:01:46 -0400

    Make ports consistent

--- trafficshifting/startDemo.sh
+++ trafficshifting/startDemo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-t="http://localhost:5000"
+t="http://localhost:8081"
 
 # start port-forward
 hp=`kubectl get pod -l service=topdogui -o name | sed 's/^pods\///'`
-kubectl port-forward $hp 5000 &
+kubectl port-forward $hp 8081:5000 &
 hpid=$!
 
 TOPDOG=$t demon
