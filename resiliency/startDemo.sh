9ab15c61322714f42522c4095b6d627b953beb24
Michael Lore <michael.lore@gmail.com>
2018-03-19 14:33:05 -0400

    Extract current namespace

--- resiliency/startDemo.sh
+++ resiliency/startDemo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+ctx=`kubectl config current-context`
+echo kube context is $ctx
+ns=`kubectl config view -o jsonpath="{.contexts[?(@.name == \"$ctx\")].context.namespace}"`
+ns=${ns:-default}
+echo KUBE_NAMESPACE is $ns
+
 w="http://localhost:8081/status/"
 h="http://localhost:8082/"
 
@@ -7,13 +13,15 @@ h="http://localhost:8082/"
 wp=`kubectl get pod -l service=webnull -o name | sed 's/^pods\///'`
 kubectl port-forward $wp 8081:8080 &
 wpid=$!
+echo WEBNULL pod is $wp
 
 # start port-forward
 hp=`kubectl get pod -l service=hurl -o name | sed 's/^pods\///'`
 kubectl port-forward $hp 8082:8080 &
 hpid=$!
+echo HURL pod is $hp
 
-WEBNULL=$w HURL=$h POD=$hp demon
+KUBE_NAMESPACE=$ns WEBNULL=$w HURL=$h POD=$hp demon
 
 kill -15 $hpid
 kill -15 $wpid
--- trafficshifting/startDemo.sh
+++ trafficshifting/startDemo.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+ctx=`kubectl config current-context`
+echo kube context is $ctx
+ns=`kubectl config view -o jsonpath="{.contexts[?(@.name == \"$ctx\")].context.namespace}"`
+ns=${ns:-default}
+echo KUBE_NAMESPACE is $ns
+
 t="http://localhost:8081"
 
 # start port-forward
 hp=`kubectl get pod -l service=topdogui -o name | sed 's/^pods\///'`
 kubectl port-forward $hp 8081:5000 &
 hpid=$!
+echo TOPDOG pod is $hp
 
-TOPDOG=$t demon
+KUBE_NAMESPACE=$ns TOPDOG=$t demon
 
 kill -15 $hpid
