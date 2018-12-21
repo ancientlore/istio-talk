6b7f377ba28b89b11b7d99e6529cd6ddf68afa9d
Michael Lore <michael.lore@gmail.com>
2018-12-21 12:23:01 -0500

    Tweak setup

--- docker/setup.ps1
+++ docker/setup.ps1
@@ -1,5 +1,5 @@
 
-echo "*** TEST KUBECTL ***"
+Write-Output "*** TEST KUBECTL ***"
 kubectl config view
 kubectl config use-context docker-for-desktop
 
@@ -8,45 +8,45 @@ kubectl config use-context docker-for-desktop
 #wait_on_istio
 
 # ingress
-echo "*** INSTALLING ISTIO ADDONS GATEWAYS ***"
-kubectl apply -f ./docker/istio/grafana-gateway.yaml
-kubectl apply -f ./docker/istio/jaeger-gateway.yaml
-kubectl apply -f ./docker/istio/servicegraph-gateway.yaml
-kubectl apply -f ./docker/istio/zipkin-gateway.yaml
-sleep 5
+Write-Output "*** INSTALLING ISTIO ADDONS GATEWAYS ***"
+kubectl apply -f .\docker\istio\grafana-gateway.yaml
+kubectl apply -f .\docker\istio\jaeger-gateway.yaml
+kubectl apply -f .\docker\istio\servicegraph-gateway.yaml
+kubectl apply -f .\docker\istio\zipkin-gateway.yaml
+Start-Sleep 5
 
 # k8s
-echo "*** SETTING KUBERNETES VARIABLES ***"
-set KUBE_NAMESPACE=default
+Write-Output "*** SETTING KUBERNETES VARIABLES ***"
+Set-Variable -name KUBE_NAMESPACE -value default
 
 # pull images
-echo "*** PULLING DOCKER IMAGES FOR LAB ***"
+Write-Output "*** PULLING DOCKER IMAGES FOR LAB ***"
 docker pull ancientlore/topdog:v0.1.2
 docker pull ancientlore/hurl:v0.1.1
 docker pull ancientlore/webnull:v0.1.1
 
-echo "*** INSTALLING DEMO CONTAINERS ***"
-istioctl kube-inject -f ./resiliency/hurl/kube/deployment.yaml > _hurl.yaml
+Write-Output "*** INSTALLING DEMO CONTAINERS ***"
+istioctl kube-inject -f .\resiliency\hurl\kube\deployment.yaml > _hurl.yaml
 kubectl apply -f _hurl.yaml
-istioctl kube-inject -f ./resiliency/webnull/kube/deployment.yaml > _webnull.yaml
+istioctl kube-inject -f .\resiliency\webnull\kube\deployment.yaml > _webnull.yaml
 kubectl apply -f _webnull.yaml
-istioctl kube-inject -f ./trafficshifting/topdog/kube/deployment-be-v1.yaml > _topdogbe1.yaml
+istioctl kube-inject -f .\trafficshifting\topdog\kube\deployment-be-v1.yaml > _topdogbe1.yaml
 kubectl apply -f _topdogbe1.yaml
-istioctl kube-inject -f ./trafficshifting/topdog/kube/deployment-be-v2.yaml > _topdogbe2.yaml
+istioctl kube-inject -f .\trafficshifting\topdog\kube\deployment-be-v2.yaml > _topdogbe2.yaml
 kubectl apply -f _topdogbe2.yaml
-istioctl kube-inject -f ./trafficshifting/topdog/kube/deployment-be-v3.yaml > _topdogbe3.yaml
+istioctl kube-inject -f .\trafficshifting\topdog\kube\deployment-be-v3.yaml > _topdogbe3.yaml
 kubectl apply -f _topdogbe3.yaml
-istioctl kube-inject -f ./trafficshifting/topdog/kube/deployment-mt-v1.yaml > _topdogmt1.yaml
+istioctl kube-inject -f .\trafficshifting\topdog\kube\deployment-mt-v1.yaml > _topdogmt1.yaml
 kubectl apply -f _topdogmt1.yaml
-istioctl kube-inject -f ./trafficshifting/topdog/kube/deployment-ui-v1.yaml > _topdogui1.yaml
+istioctl kube-inject -f .\trafficshifting\topdog\kube\deployment-ui-v1.yaml > _topdogui1.yaml
 kubectl apply -f _topdogui1.yaml
-rm _*.yaml
+Remove-Item _*.yaml
 
-echo "*** INSTALLING DEMO GATEWAYS ***"
-kubectl apply -f ./docker/istio/demo-gateway.yaml
+Write-Output "*** INSTALLING DEMO GATEWAYS ***"
+kubectl apply -f .\docker\istio\demo-gateway.yaml
 
-echo "*** ADDING ISTIO VIRTUAL SERVICES ***"
-istioctl create -f ./trafficshifting/istio/services-all-v1.yaml -n default
-istioctl create -f ./resiliency/istio/services.yaml -n default
+Write-Output "*** ADDING ISTIO VIRTUAL SERVICES ***"
+istioctl create -f .\trafficshifting\istio\services-all-v1.yaml -n default
+istioctl create -f .\resiliency\istio\services.yaml -n default
 
-echo 'echo "Run ./demo.sh to start the demo."'
+Write-Output 'echo "Run .\demo.sh to start the demo. You need the bash shell."'
